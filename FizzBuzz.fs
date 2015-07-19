fb7b9f71b7858d613654edde7accf2b3123a9800
Andrew Hamblin <tori@ticviking.com>
2015-07-18 19:46:53 -0600

    [Green] Kata finished in 1h20m

--- FizzBuzz.fs
+++ FizzBuzz.fs
@@ -2,6 +2,7 @@ module FizzBuzz
 
 let fizzBuzz n =
   match n with
+    | n when ((n % 5) = 0 ) && ((n%3)=0)-> "fizzbuzz"
     | n when ((n % 5) = 0 )-> "buzz"
     | n when ((n % 3) = 0 )-> "fizz"
     | n -> sprintf "%d" n
